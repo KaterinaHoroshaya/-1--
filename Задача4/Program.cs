6357ddb226b6e366048278c1feddc1f68bf86d19
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-15 02:24:22 +0700

    Задача4

---
+++ Задача4/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите положительное число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+int b = 2;
+int count = 2;
+
+while (count<=a)
+{
+    Console.WriteLine($"{b}");
+    b=b+2;
+    count=count+2;
+}
